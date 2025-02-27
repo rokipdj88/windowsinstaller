12a16cb3d27b32f9f5bbf6832cbb6631450e1a75
rokipdj88 <candraxr4@gmail.com>
2025-02-27 11:09:06 +0700

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+echo -e '\e[34m'
+echo -e '$$\   $$\ $$$$$$$$\      $$$$$$$$\           $$\                                       $$\     '
+echo -e '$$$\  $$ |\__$$  __|     $$  _____|          $$ |                                      $$ |    '
+echo -e '$$$$\ $$ |   $$ |        $$ |      $$\   $$\ $$$$$$$\   $$$$$$\  $$\   $$\  $$$$$$$\ $$$$$$\   '
+echo -e '$$ $$\$$ |   $$ |$$$$$$\ $$$$$\    \$$\ $$  |$$  __$$\  \____$$\ $$ |  $$ |$$  _____|\_$$  _|  '
+echo -e '$$ \$$$$ |   $$ |\______|$$  __|    \$$$$  / $$ |  $$ | $$$$$$$ |$$ |  $$ |\$$$$$$\    $$ |    '
+echo -e '$$ |\$$$ |   $$ |        $$ |       $$  $$<  $$ |  $$ |$$  __$$ |$$ |  $$ | \____$$\   $$ |$$\ '
+echo -e '$$ | \$$ |   $$ |        $$$$$$$$\ $$  /\$$\ $$ |  $$ |\$$$$$$$ |\$$$$$$  |$$$$$$$  |  \$$$$  |'
+echo -e '\__|  \__|   \__|        \________|\__/  \__|\__|  \__| \_______| \______/ \_______/    \____/ '
+echo -e '\e[0m'
+echo -e "Join our Telegram channel: https://t.me/NTExhaust"
+sleep 5
+
+
+# Function to display menu and get user choice
+display_menu() {
+    echo "Please select the Windows Server or Windows version:"
+    echo "1. Windows Server 2016"
+    echo "2. Windows Server 2019"
+    echo "3. Windows Server 2022"
+    echo "4. Windows 10"
+    echo "5. Windows 11"
+    echo "6. Windows 1021h2"
+    read -p "Enter your choice: " choice
+}
+
+# Update package repositories and upgrade existing packages
+apt-get update && apt-get upgrade -y
+
+# Install QEMU and its utilities
+apt-get install qemu -y
+apt install qemu-utils -y
+apt install qemu-system-x86-xen -y
+apt install qemu-system-x86 -y
+apt install qemu-kvm -y
+
+# Open port 5900 for VNC access
+ufw allow 5900/tcp
+
+echo "QEMU installation completed successfully. Port 5900 opened for VNC."
+
+# Get user choice
+display_menu
+
+case $choice in
+    1)
+        img_file="windows2016.img"
+        iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195174&clcid=0x409&culture=en-us&country=US"
+        iso_file="windows2016.iso"
+        ;;
+    2)
+        img_file="windows2019.img"
+        iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195167&clcid=0x409&culture=en-us&country=US"
+        iso_file="windows2019.iso"
+        ;;
+    3)
+        img_file="windows2022.img"
+        iso_link="https://go.microsoft.com/fwlink/p/?LinkID=2195280&clcid=0x409&culture=en-us&country=US"
+        iso_file="windows2022.iso"
+        ;;
+    4)
+        img_file="windows10.img"
+        iso_link="http://134.199.163.87/WIN10.iso"
+        iso_file="windows10.iso"
+        ;;
+    5)
+        img_file="windows11.img"
+        iso_link="http://134.199.163.87/WIN11.iso"
+        iso_file="windows11.iso"
+        ;;
+    6)
+        img_file="windows1021h2.img"
+        iso_link="http://134.199.163.87/win1021H2.img"
+        iso_file="windows1021h2.iso"
+        ;;
+    *)
+        echo "Invalid choice. Exiting."
+        exit 1
+        ;;
+esac
+
+echo "Selected version: $img_file"
+
+# Create a raw image file with the chosen name
+qemu-img create -f raw "$img_file" 40G
+
+echo "Image file $img_file created successfully."
+
+# Download Virtio driver ISO
+wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
+
+echo "Virtio driver ISO downloaded successfully."
+
+# Download Windows ISO with the chosen name
+wget -O "$iso_file" "$iso_link"
+
+echo "Windows ISO downloaded successfully."
+
+# Start QEMU instance
+qemu-system-x86_64 \
+    -m 4G \
+    -cpu host \
+    -enable-kvm \
+    -boot order=d \
+    -drive file="$iso_file",media=cdrom \
+    -drive file="$img_file",format=raw,if=virtio \
+    -drive file=virtio-win.iso,media=cdrom \
+    -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
+    -device usb-tablet \
+    -vnc :0
